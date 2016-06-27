ebfaf1c1a3951fc38216e9c345fc267cf4f5e756
Tomas Jancar <jancar@modpreneur.com>
2016-06-27 10:42:34 +0200

    add BillingPlan

---
+++ BillingPlanInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Trinity\Component\Core\Interfaces;
+
+/**
+ * Interface BillingPlanInterface
+ * @package Trinity\Component\Core\Interfaces
+ */
+interface BillingPlanInterface
+{
+
+    public function getId();
+
+
+    public function getFrequency();
+
+
+    public function getInitialPrice();
+
+
+    public function getProduct();
+
+
+    public function getRebillPrice();
+
+
+    public function getRebillTimes();
+
+
+    public function getTrial();
+
+
+    public function isRecurring();
+}
